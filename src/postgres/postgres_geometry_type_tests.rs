9f6a76aace260d08fda8c63685f050b9a185bc1c
Pavlos-Petros Tournaris <p.tournaris@gmail.com>
2025-06-05 11:30:38 +0300

    Allow configurable SRID for geometry types

--- src/postgres/postgres_geometry_type_tests.rs
+++ src/postgres/postgres_geometry_type_tests.rs
@@ -4,13 +4,13 @@ mod tests {
 
     #[test]
     fn test_is_geometry_type() {
-        let geometry_type = PostgresGeometryType::new("MULTIPOLYGON(");
+        let geometry_type = PostgresGeometryType::new("MULTIPOLYGON(", 0);
         assert!(geometry_type.is_geometry_type());
     }
 
     #[test]
     fn test_is_not_geometry_type() {
-        let geometry_type = PostgresGeometryType::new("POINT(");
+        let geometry_type = PostgresGeometryType::new("POINT(", 0);
         assert!(!geometry_type.is_geometry_type());
     }
 }
